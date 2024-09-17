f4034ef5185b0c3e41fb622c58f63c523454818b
tlin <tlin>
2024-09-17 17:54:46 -0500

    added newline to "Hello and welcome" message.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,8 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+// git comments
+// 1. initial commit
+// 2. added new line to the hello and welcome message
 public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
