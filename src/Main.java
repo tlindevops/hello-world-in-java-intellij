81a6a434fe75af100c073763e3738572c447219f
tlin <tlin>
2024-09-17 17:57:03 -0500

    added newline to "Hello and welcome" message.

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
     public static void main(String[] args) {
         // Press Opt+Enter with your caret at the highlighted text to see how
         // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!\n");
+        System.out.printf("Hello and welcome!\n\n");
 
         // Press Ctrl+R or click the green arrow button in the gutter to run the code.
         for (int i = 1; i <= 5; i++) {
